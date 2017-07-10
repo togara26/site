8ecc0749f096be2e0c70040f7090a51d346520c7
Tyler M. O'Gara <xxogaraxx@gmail.com>
2017-07-09 20:37:09 -0500

    Initial setup

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Third party service providers
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+
     ],
 
     /*
